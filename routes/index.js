77303c87401094babe5697ced02ba330f5e8d07d
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-12-13 00:48:05 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
+import AuthController from '../controllers/AuthController.js';
 import UsersController from '../controllers/UsersController.js';
+import FilesController from '../controllers/FilesController.js';
 
 const router = Router();
 
@@ -8,7 +10,12 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// UsersController route
-router.post('/users', UsersController.postNew);
+// AuthController and UsersController routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
+
+// New FilesController route for file upload
+router.post('/files', FilesController.postUpload);
 
 export default router;
