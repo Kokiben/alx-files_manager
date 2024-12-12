8ef43233f950c9c7d35ec47697af4a71467e5233
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-12-13 00:34:54 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
-import AuthController from '../controllers/AuthController.js';
 import UsersController from '../controllers/UsersController.js';
 
 const router = Router();
@@ -9,9 +8,7 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// New AuthController and UsersController routes
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+// UsersController route
+router.post('/users', UsersController.postNew);
 
 export default router;
